7c5f2939adf8c5546496443f5b112a3e35c6c05b
Rishabh Tomar <rishabhtomar9999@gmail.com>
2025-06-22 18:53:25 +0530

    feat: add dotenv for environment variable management; refactor firebase config to use environment variables

--- RecipeMakerProject/src/firebase.js
+++ RecipeMakerProject/src/firebase.js
@@ -1,14 +1,15 @@
+// src/firebase.js
 import { initializeApp } from 'firebase/app'
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA0j3Wbp289D4-Ah1RtYtrUh_O_ByK9FAw",
-  authDomain: "cooksync-ff36a.firebaseapp.com",
-  projectId: "cooksync-ff36a",
-  storageBucket: "cooksync-ff36a.firebasestorage.app",
-  messagingSenderId: "617733988322",
-  appId: "1:617733988322:web:80e76d792c1e9cfba8a3bb"
-};
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+}
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
