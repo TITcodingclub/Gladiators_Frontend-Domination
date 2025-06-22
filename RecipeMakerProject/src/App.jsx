9396079dfee094b921e5cb4f068497eec99bb2a2
Rishabh Tomar <rishabhtomar9999@gmail.com>
2025-06-22 11:42:06 +0530

    feat: add Navbar component and update App to include it; add react-icons dependency

--- RecipeMakerProject/src/App.jsx
+++ RecipeMakerProject/src/App.jsx
@@ -1,8 +1,10 @@
 import RecipePage from './pages/RecipePage'
+import Navbar from './components/Navbar'
 
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-100">
+      <Navbar />
       <RecipePage />
     </div>
   )
