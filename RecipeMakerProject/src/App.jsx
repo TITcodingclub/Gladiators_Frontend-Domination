dcec1a4550d0f72bb1f3bdd2ca228325f7b5a7d6
samiksha suryawanshi <suryawanshisamiksha506@gmail.com>
2025-06-22 11:57:28 +0530

    fix: update App background colors for better contrast; add color comments in CSS

--- RecipeMakerProject/src/App.jsx
+++ RecipeMakerProject/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-100">
+    <div className="min-h-screen bg-gray-50 dark:bg-[#2A2C41] text-[#2A2C41] dark:text-gray-100">
       <Navbar />
       <RecipePage />
     </div>
